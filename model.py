37a04cd27a870ffff173b325d18eb75c8c1dc4f3
Julie Stephany Berrio Perez <jber4282@sydney.edu.au>
2020-07-28 15:27:14 +1000

    first commit

---
+++ model.py
@@ -0,0 +1,33 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('new_data/driving_log.csv') as csv_file:
+    reader = csv.reader(csv_file)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+
+for line in lines:
+    source_path = line[0]
+    image = cv2.imread(source_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+Y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.save('model.h5')
