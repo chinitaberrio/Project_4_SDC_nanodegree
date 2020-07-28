fde80f09a06d5c87ef9f4be6bfd6ec50009a8972
Julie Stephany Berrio Perez <jber4282@sydney.edu.au>
2020-07-29 08:46:13 +1000

    working version

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import csv
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
 
 lines = []
 with open('new_data/driving_log.csv') as csv_file:
@@ -18,16 +20,54 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
-X_train = np.array(images)
-Y_train = np.array(measurements)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement*-1.0)
+
+X_train = np.array(augmented_images)
+Y_train = np.array(augmented_measurements)
+
+print(X_train.shape)
+print(Y_train.shape)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
+# Creating the model
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+# Normalizing the data
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
+# Cropping non relevant information
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+'''print(history_object.history.keys())
+
+# plotting the history of the training
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()'''
+
+
 model.save('model.h5')
+exit()
